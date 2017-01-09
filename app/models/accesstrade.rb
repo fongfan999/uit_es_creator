a9bf39751a858b7f28bafb7edbabe16d199e7d6c
Phong Phan <fongfan999@gmail.com>
2017-01-09 18:11:23 +0700

    Fixed typo in Accesstrade class

--- app/models/accesstrade.rb
+++ app/models/accesstrade.rb
@@ -5,14 +5,15 @@ class Accesstrade
 
   def self.generate_short_link(origin_link)
     # Get link and parse to json
-    JSON.parse (
+    JSON.parse(
       post('http://api.accesstrade.vn/me/product_links', {
         body: {
-          access_key: ENV['AT_ACCESS_KEY'],
+          # access_key: ENV['AT_ACCESS_KEY'],
+          access_key: "BDWnWb-g6SJ534r-jSwcMP_mvhcznH_0",
           url: origin_link,
           utm_source: "facebook"
         }
-      }).body['short_link']
-    )
+      }).body
+    )['short_link']
   end
 end
