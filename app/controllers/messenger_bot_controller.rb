1104b5493bb769a4e720da3479fd6937a83941f4
Phong Phan <fongfan999@gmail.com>
2017-01-09 18:00:52 +0700

    Remove checking url condition

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -2,13 +2,9 @@ class MessengerBotController < ActionController::Base
   def message(event, sender)
     origin_link = event['message']['text']
 
-    if origin_link =~ /http/
-      sender.reply({ text: "How are you doing today. Please be patient!" })
-      sender.reply({ text: "Short link: #{Accesstrade.generate_short_link(origin_link)}" })
-      sender.reply({ text: "Product link: https://pub.accesstrade.vn/deep_link/4505309943460540043?url=#{origin_link}&utm_source=facebook" })
-    else
-      sender.reply({ text: "Đường dẫn không hợp lệ. Vui lòng nhập lại một đường dẫn khác!" })
-    end
+    sender.reply({ text: "How are you doing today. Please be patient!" })
+    sender.reply({ text: "Short link: #{Accesstrade.generate_short_link(origin_link)}" })
+    sender.reply({ text: "Product link: https://pub.accesstrade.vn/deep_link/4505309943460540043?url=#{origin_link}&utm_source=facebook" })
   end
 
   def delivery(event, sender)
