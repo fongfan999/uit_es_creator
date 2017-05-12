7593df085caee3c316d9a3bbc8ffa4c88822beea
Phong Phan <fongfan999@gmail.com>
2017-05-12 11:56:21 +0700

    Add API

--- app/controllers/api/v1/daa_controller.rb
+++ app/controllers/api/v1/daa_controller.rb
@@ -1,11 +1,9 @@
 class Api::V1::DaaController < Api::V1::ApplicationController
   def uesc
-    @students = JSON.parse File.read(Rails.root.join('public/students.json'))
-    
-    if student = @students[params[:student_id]] || {}
+    if student = Student.find_by_student_id(params[:student_id]) || {}
       result = {
-        name: student['name'],
-        es: student['class_ids']
+        name: student.name,
+        es: student.klasses
       }
     end
 
