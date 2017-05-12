575f4d3c685365e5c7fd01c6c4e85be788a51159
Phong Phan <fongfan999@gmail.com>
2017-05-12 12:36:18 +0700

    Request to get data instead query from DB

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
   def index
     session[:student_id] = params[:student_id] || session[:student_id]
 
-    if @student = Student.find_by_student_id(session[:student_id])
-      @classes = @student.klasses
+    if @student = Student.find_student(session[:student_id])
+      @classes = @student['es']
 
       Tracker.track(session[:student_id])
     end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,11 @@
+require 'httparty'
+
 class Student < ApplicationRecord
+  API_URL = 'http://188.166.237.215:37209/api/v1/daa/uesc.json'
   has_many :exam_schedulers
   has_many :klasses, through: :exam_schedulers
+
+  def self.find_student(student_id)
+    JSON.parse HTTParty.get(API_URL + "?student_id=#{student_id}").body
+  end
 end
