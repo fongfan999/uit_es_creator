5f9519c4694598d9a561ba52cf889d40dd5f9e97
Phong Phan <fongfan999@gmail.com>
2017-05-17 19:08:20 +0700

    Fixed exam schedulers order

--- app/models/student.rb
+++ app/models/student.rb
@@ -17,9 +17,9 @@ class Student < ApplicationRecord
 
   private
     def klasses_with_id_number
-      exam_schedulers.map do |es|
-        Klass.find(es.klass_id).attributes
-          .except('created_at', 'updated_at')
+      ExamScheduler.includes(:klass)
+        .where('student_id = ?', self.id).map do |es|
+        es.klass.attributes.except('created_at', 'updated_at')
           .merge({id_number: es.id_number})
       end
     end
