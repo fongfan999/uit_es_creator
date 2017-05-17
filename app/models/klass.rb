69e3c94ab589352360b2c7fb756275a54a78663f
Phong Phan <fongfan999@gmail.com>
2017-05-17 18:38:27 +0700

    Order by shift if the dates are same

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -1,5 +1,5 @@
 class Klass < ApplicationRecord
-  default_scope { order(:date) }
+  default_scope { order(:date, :shift) }
 
   has_many :exam_schedulers
   has_many :students, through: :exam_schedulers
