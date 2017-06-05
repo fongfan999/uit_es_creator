cb1948798c7662ffd321457b6b726be9e39ffae7
Phong Phan <fongfan999@gmail.com>
2017-06-05 12:24:20 +0700

    Add tracker count

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,4 +11,9 @@ class HomeController < ApplicationController
       Tracker.track(session[:student_id])
     end
   end
+
+  private
+    def load_shifts
+      @shifts ||= { "1": "7:30", "2": "9:30", "3": "13:30", "4": "15:30" }
+    end
 end
