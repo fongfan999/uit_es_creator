f8c779f5afbe844f017636b23c353bf56aa2e017
Phong Phan <fongfan999@gmail.com>
2017-06-08 07:14:57 +0700

    Applied regex to student id query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
   def index
     session[:student_id] = params[:student_id] || session[:student_id]
-    @student = Student.find_student(session[:student_id])
+    @student = Student.find_student(session[:student_id][/\d{8}/])
 
     unless @student.empty?
       @classes = @student['es']
