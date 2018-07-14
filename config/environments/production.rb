532de2eae05a06add4b3a979e1040ec2cdb8edda
Phong Phan <fongfan999@gmail.com>
2018-07-14 20:51:14 +0700

    Allow CORS for API

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,10 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => %w(GET POST OPTIONS).join(',')
+  }
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
